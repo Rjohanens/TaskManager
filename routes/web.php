db47ae22cb4a9007ffe668be7318d8cd06d3e3cf
rjohannes <rick@appademic.nl>
2026-03-22 22:06:19 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'show'])->name('login');
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('authenticate');
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
